88adf98cbab50b683623ea0e96a29ffd1017555d
asifmt786 <asifmt786@yahoo.co.uk>
2018-12-11 17:31:16 +0100

    Tested last part of main in last part

--- TIS_Main_Test.java
+++ TIS_Main_Test.java
@@ -1,4 +1,18 @@
 public class TIS_Main_Test {
   private static TIS_Main main = new TIS_Main();
 
+
+  private static boolean test_printWelcome(){
+  return ("\n    Welcome to our ticket machine!" == main.callPrintWelcome());
+  }
+
+
+  public static void main(String[] args){
+    if (test_printWelcome()){
+      System.out.println("PASSED");
+    } else {
+      System.out.println("FAILED");
+    }
+  }
+
 }
