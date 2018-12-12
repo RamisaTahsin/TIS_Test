9086c7fdb2783f2610bbacbc08a07d70546169b5
Karin Sörlin <sorlin.karin@gmail.com>
2018-12-12 18:38:46 +0100

    Test code for TIS_Transaction_Test.java file

--- TIS_Transaction_Test.java
+++ TIS_Transaction_Test.java
@@ -1,4 +1,17 @@
 public class TIS_Transaction_Test {
-  private static TIS_Transaction trans = new TIS_Transaction();
+  private static String accountNr = "123";
+  private static double price = 10;
 
+  private static boolean test_Transaction(){
+    TIS_TransactionStatus result = TIS_Transaction.makeTransaction(accountNr, price);
+      return result.getSuccessful() && result.getBank().equals("SEB");
+
+  }
+  public static void main(String[] args){
+
+    if (test_Transaction())
+    System.out.println("test_Transaction True");
+    else
+    System.out.println("test_Transaction False");
+  }
 }
