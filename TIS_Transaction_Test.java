f82a014b8e9545d5c03222aa4df9b743f5bc80ac
Karin Sörlin <sorlin.karin@gmail.com>
2018-12-13 19:30:51 +0100

    Main test shows passed, and minor small changes

--- TIS_Transaction_Test.java
+++ TIS_Transaction_Test.java
@@ -2,16 +2,17 @@ public class TIS_Transaction_Test {
   private static String accountNr = "123";
   private static double price = 10;
 
-  private static boolean test_Transaction(){
+  private static boolean test_Transaction() {
     TIS_TransactionStatus result = TIS_Transaction.makeTransaction(accountNr, price);
       return result.getSuccessful() && result.getBank().equals("SEB");
 
   }
-  public static void main(String[] args){
+  
+  public static void main(String[] args) {
 
     if (test_Transaction())
-    System.out.println("test_Transaction True");
+    System.out.println("test_Transaction PASSED");
     else
-    System.out.println("test_Transaction False");
+    System.out.println("test_Transaction FAILED");
   }
 }
